555c4428ab288864eca0e9b1a49c1276c17422a7
Mengyao Shi <katherinesmy@debian>
2017-12-10 16:49:44 -0800

    horizontal

--- energy_image.jl
+++ energy_image.jl
@@ -5,14 +5,14 @@ Gx_, Gy_=imgradients(gray, "sobel")
 energy=sqrt(Gx_.^2+Gy_.^2)
 (height, width)=size(energy)
 cumulativeEnergyMap=energy
-for i=2:height
-        for j=1:width
-            if j==1
-                cumulativeEnergyMap[i,j]=cumulativeEnergyMap[i,j]+min(cumulativeEnergyMap[i-1,j], cumulativeEnergyMap[i-1,j+1])
-            elseif j==width
-                cumulativeEnergyMap[i,j]=cumulativeEnergyMap[i,j]+min(cumulativeEnergyMap[i-1,j], cumulativeEnergyMap[i-1,j-1])
+for j=2:width
+        for i=1:height
+            if i==1
+                cumulativeEnergyMap[i,j]=cumulativeEnergyMap[i,j]+min(cumulativeEnergyMap[i, j-1], cumulativeEnergyMap[i+1, j-1])
+            elseif i==height
+                cumulativeEnergyMap[i,j]=cumulativeEnergyMap[i,j]+min(cumulativeEnergyMap[i, j-1], cumulativeEnergyMap[i-1, j-1])
             else
-                cumulativeEnergyMap[i,j]=cumulativeEnergyMap[i,j]+min(cumulativeEnergyMap[i-1,j-1], cumulativeEnergyMap[i-1,j], cumulativeEnergyMap[i-1,j+1])
+                cumulativeEnergyMap[i,j]=cumulativeEnergyMap[i,j]+min(cumulativeEnergyMap[i-1,j-1], cumulativeEnergyMap[i, j-1], cumulativeEnergyMap[i+1, j-1])
             end
         end
 end
